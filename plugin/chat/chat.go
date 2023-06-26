ecad874dcc7ef51fcaba47299d3ca0368d95118e
yeying99 <108513820+yeying99@users.noreply.github.com>
2023-06-27 01:35:51 +0800

    Update chat.go

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -26,7 +26,6 @@ func init() { // 插件主体
 	// 被喊名字
 	engine.OnFullMatch("蛇蛇", zero.OnlyToMe).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			var nickname = zero.BotConfig.NickName[0]
 			time.Sleep(time.Second * 1)
 			ctx.SendChain(message.Text(
 				[]string{
@@ -40,7 +39,6 @@ func init() { // 插件主体
 	// 戳一戳
 	engine.On("notice/notify/poke", zero.OnlyToMe).SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
-			var nickname = zero.BotConfig.NickName[0]
 			switch {
 			case poke.Load(ctx.Event.GroupID).AcquireN(3):
 				// 5分钟共8块命令牌 一次消耗3块命令牌
